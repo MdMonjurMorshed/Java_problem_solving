81da203df4d80dfe2940893b377f7d04ffd5c212
MD MONJUR MORSHED <mmt3719@gmail.com>
2024-01-15 13:05:16 +0600

    gcd problem

--- gcd_of_string.java
+++ gcd_of_string.java
@@ -3,20 +3,7 @@ public class gcd_of_string{
         System.out.println(str1 + " " +  str2);
         int min_length=Math.min(str1.length(),str2.length());
         System.out.println(min_length);
-        // if(str1+str2!=str2+str1){
-
-        //     return "";
-        // }
-        // if(str1==str2){
-        //     System.out.println("str1 and str2 is equal");
-        //     return str1;
-        // }else if(str1.length()>str2.length()){
-        //     System.out.println(str1.substring(0, str2.length()));
-        //     return gcd_string(str1.substring(0, str2.length()), str2);
-        // }else{
-        //     System.out.println(str2.substring(0, str1.length()));
-        //     return  gcd_string(str2.substring(0, str1.length()), str1);
-        // }
+       
         StringBuilder  str_build= new StringBuilder();
         for (int i=0;i<min_length; i++){
             if (str1.charAt((i))==str2.charAt(i)){
